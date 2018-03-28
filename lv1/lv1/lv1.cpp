f8143d87ad4d7dca5647b0e442087ecb24507adb
ahmura <antonio.hmura@gmail.com>
2018-03-28 13:46:02 +0200

    moj prvi commit

---
+++ lv1/lv1/lv1.cpp
@@ -0,0 +1,14 @@
+// lv1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Pozdrav iz aplikacije od Antonio Hmura" << endl;
+    return 0;
+}
+
